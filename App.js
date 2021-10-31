77bb608f96eb5b30ad5eb0bd1d2aa1172f846571
AnchalSheemar <53333322+AnchalSheemar@users.noreply.github.com>
2021-10-31 16:31:57 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import services from'./assets/images/services.png';
+
+function App() {
+  return (
+    <div className="App">
+<div className="bg_cover">
+<h3 className="header">Handcrafted Landing Page for <br/>Startups and SaaS Businesses</h3>
+<p className="text">A simple, customizable, and, beautiful SaaS business focused landing page to make
+ <br/>your project closer to launch!</p>
+
+<h4 className="title">Crafted For</h4>
+<p class="text">Stop wasting time and money designing and managing a 
+<br/>website that doesn’t get results. Happiness guaranteed!</p><br/><br/>
+
+ </div>
+    </div>
+  );
+}
+
+export default App;
